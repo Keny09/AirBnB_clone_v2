7382dc4487c4ec683e430c74032d5daddd703755
Keny09 <rosekendy09@gmail.com>
2023-10-24 20:00:37 -0700

    web flask update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""my simple Web Server API application module"""
+
+"""A python script that uses web flask to host web application
+"""
+
 from flask import Flask
+import os
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_flask():
-    """my first flask application"""
-    return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello():
+    """A function that displays greetings"""
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
-def hello_hbnb():
-    """function take the path variable"""
-    return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """A function that displays hbnb"""
+    return 'HBNB'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    os.environ['FLASK_ENV'] = 'development'
+    app.run(host='0.0.0.0', port=5000)
